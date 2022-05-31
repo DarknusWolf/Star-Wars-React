8aa48c6194d3bbb95e263b9106c7d1744670761e
DarknusWolf <girelagarcia.javier29@gmail.com>
2022-05-31 03:42:27 +0200

    incluyendo rutas

--- src/containers/ListaPersonajes/ListaPersonajes-style.js
+++ src/containers/ListaPersonajes/ListaPersonajes-style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const ListaPersonajesContainer = styled.ul`
+    padding: 40px;
     display: flex;
     justify-content: space-between;
     align-items: flex-start;
